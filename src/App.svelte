883ed07bfd74f63ba4d378da396eded5a195fd0e
Kevin Canlas <kvncnlsdsgns@gmail.com>
2021-05-18 21:29:08 -0400

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import CoinCard from "./CoinCard.svelte";
+
+	let name = "Crypto Tracker";
+
+	let coins = [];
+
+	async function fetchCoins(){
+		const response = await fetch("https://api.coinstats.app/public/v1/coins?skip=0&limit=50");
+		const data = await response.json();
+		coins = data.coins;
+
+		console.log("coins:", coins);
+	}
+</script>
+
+<main>
+	<h1>{name}</h1>
+	<button on:click="{fetchCoins}">Fetch Coin Data!</button>
+
+	{#each coins as coin}
+		<CoinCard coinProp={coin}/>
+	{/each}
+
+</main>
+
+<style lang="scss">
+
+</style>
---
+++ src/CoinCard.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let coinProp;
+</script>
+
+<div class="coin-card">
+    <img src="{coinProp.icon}" alt="" class="coin-image">
+    <p class="coin-name">{coinProp.name}</p>
+    <div class="coin-details">
+        <div class="detail-field price-change">
+            {coinProp.priceChange1w}%
+        </div>
+        <div class="detail-field">{coinProp.price.toFixed(2)}%</div>
+        <a href={coinProp.websiteUrl} class="detail-field">Website &#8594;</a>
+    </div>
+</div>
+
+<style lang="scss"> // Styling is scoped to each component
+
+</style>
